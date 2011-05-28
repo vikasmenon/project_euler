44ebb16214b6dcadf99c3e33cddda65f8b0a9720
Vikas Menon <menonvikas@gmail.com>
2011-05-27 22:00:42 -0400

    Problem 1: solved

--- elisp/1.el
+++ elisp/1.el
@@ -1,7 +1,13 @@
 
-(def sum_all x (/ (* x x+1) 2))
+(defun sum_all (x)
+  (/ (* x (+ x 1)) 2))
 
-(def modular x y ( y * sum_all((/ (- x 1) y))))
+(defun modular (x y)
+  ( * y (sum_all (/ (- x 1) y))))
 
-(def awesome x (- (+ (modular x 3) (modular x 5)) (modular x 15)))
+(defun awesome (x)
+  (- (+ (modular x 3) (modular x 5)) (modular x 15)))
+
+
+(awesome 1000)
 
